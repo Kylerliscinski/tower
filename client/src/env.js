73a0f56980800fdfffa5fd85460d8fee293909ff
Kylerliscinski <62256404+Kylerliscinski@users.noreply.github.com>
2024-04-23 14:29:12 -0600

    Init

---
+++ client/src/env.js
@@ -0,0 +1,8 @@
+export const dev = window.location.origin.includes('localhost')
+export const baseURL = dev ? 'http://localhost:3000' : ''
+export const useSockets = false
+export const domain = 'dev-igepbtkmc1ba5lje.us.auth0.com'
+export const clientId = 'aeNaB4HPylNWypTUZCNFcKeYzOVpjfx3'
+export const audience = 'https://houses.com'
+
+
